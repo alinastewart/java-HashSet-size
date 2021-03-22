501a7d84c26a45285c11b71ecd7bedfe96e31b9a
Alina Stewart <alinastewart@Alinas-iMac.local>
2021-03-22 16:41:16 -0700

    Output the size of given HashSet.

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+
+// Import the HashSet class
+import java.util.HashSet;
+
+public class Main {
+  public static void main(String[] args) {
+    HashSet<String> flowers = new HashSet<String>();
+    flowers.add("Daisy");
+    flowers.add("Tulip");
+    flowers.add("Rose");
+    flowers.add("Daffodil");
+    flowers.add("Aster");
+    System.out.println(flowers.size());
+  }
+}
